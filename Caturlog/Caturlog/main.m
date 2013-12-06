36c67f5c3942b88b9904fd84ede4bef27f555e71
Jon Rubin <jb.rubin@gmail.com>
2013-12-06 15:17:45 -0500

    Initial empty project commit.

---
+++ Caturlog/Caturlog/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Caturlog
+//
+//  Created by Jonathon Rubin on 12/6/13.
+//  Copyright (c) 2013 Jonathon Rubin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
---
+++ Caturlog/CaturlogImporter/MySpotlightImporter.h
@@ -0,0 +1,19 @@
+//
+//  MySpotlightImporter.h
+//  CaturlogImporter
+//
+//  Created by Jonathon Rubin on 12/6/13.
+//  Copyright (c) 2013 Jonathon Rubin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MySpotlightImporter : NSObject
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (BOOL)importFileAtPath:(NSString *)filePath attributes:(NSMutableDictionary *)attributes error:(NSError **)error;
+
+@end
