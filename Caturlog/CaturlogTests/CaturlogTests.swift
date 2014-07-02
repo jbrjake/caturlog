0e26801d0efc00c91df8d303bcd037944e9df4fe
Jon Rubin <jb.rubin@gmail.com>
2014-07-01 23:27:10 -0400

    Sigh. StackMob is no more. Recreating, this time with Swift, and with a base Mac Core Data project. Let's try getting basic functionality implemented before worrying about networks and multiple users. The CD model file's been recreated based on the old one.

---
+++ Caturlog/CaturlogTests/CaturlogTests.swift
@@ -0,0 +1,35 @@
+//
+//  CaturlogTests.swift
+//  CaturlogTests
+//
+//  Created by Jonathon Rubin on 7/1/14.
+//  Copyright (c) 2014 Jonathon Rubin. All rights reserved.
+//
+
+import XCTest
+
+class CaturlogTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
