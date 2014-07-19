828bfeff98e113eec37a36f83c235077dec6f65b
Jon Rubin <jb.rubin@gmail.com>
2014-07-18 23:06:27 -0400

    Splits off the Omnibar code into its own view controller.

--- Caturlog/Caturlog/CaturlogWindowController.swift
+++ Caturlog/Caturlog/CaturlogWindowController.swift
@@ -11,7 +11,6 @@ import AppKit
 
 class CaturlogWindowController : NSWindowController {
 
-    @IBOutlet var omnibar :NSTokenField
     @IBOutlet var itemTable :NSTableView
     @IBOutlet var imageView :NSImageView
     @IBOutlet var tagField :NSTokenField
@@ -19,47 +18,5 @@ class CaturlogWindowController : NSWindowController {
     override func windowWillLoad() {
         super.windowWillLoad()
     }
-    
-    override func controlTextDidEndEditing(obj: NSNotification!) {
-        var textField :NSTextField = obj.object as NSTextField
-        if( textField.isEqualTo(omnibar) ) {
-            let tokenField = textField as NSTokenField
-            let tokens = tokenField.objectValue as Array<String>
-            let (urls,tags) = urlsAndTagsFromTokens(tokens)
-            for url in urls {
-                addResource(url)
-            }
-        }
-    }
-
-    func urlsAndTagsFromTokens(tokens :Array<String>) -> (Array<NSURL>,Array<String>) {
-        var urls = Array<NSURL>()
-        var tags = Array<String>()
-        for tokenString in tokens {
-            if let url = NSURL.URLWithString(tokenString) {
-                urls.append(url)
-            }
-            else {
-                tags.append(tokenString)
-            }
-        }
-        return(urls, tags)
-    }
-    
-    func addResource(url: NSURL) {
-        var appDel = NSApplication.sharedApplication().delegate as AppDelegate
-        var services = appDel.caturlogServices
-        services.resourceLoader.getResource(url, completion: {
-            data in
-            if data != nil {
-                services.resourceStorer.storeResource(data!.sha256(), fromURL:url)
-            }
-        })
-    }
-    
-}
-
-
-extension CaturlogWindowController : NSTokenFieldDelegate {
     
 }
