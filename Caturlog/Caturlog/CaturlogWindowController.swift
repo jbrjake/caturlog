b56a78c149300baec4e4393126f9e99735f16c93
Jon Rubin <jb.rubin@gmail.com>
2014-07-18 22:56:57 -0400

    Separates out the omnibar contents into tokens and then URLs and tags.

--- Caturlog/Caturlog/CaturlogWindowController.swift
+++ Caturlog/Caturlog/CaturlogWindowController.swift
@@ -22,13 +22,30 @@ class CaturlogWindowController : NSWindowController {
     
     override func controlTextDidEndEditing(obj: NSNotification!) {
         var textField :NSTextField = obj.object as NSTextField
-        if( textField.isEqualTo(omnibar)) {
-            if let url = NSURL.URLWithString(omnibar.stringValue) {
-                addResource(url)                
+        if( textField.isEqualTo(omnibar) ) {
+            let tokenField = textField as NSTokenField
+            let tokens = tokenField.objectValue as Array<String>
+            let (urls,tags) = urlsAndTagsFromTokens(tokens)
+            for url in urls {
+                addResource(url)
             }
         }
     }
 
+    func urlsAndTagsFromTokens(tokens :Array<String>) -> (Array<NSURL>,Array<String>) {
+        var urls = Array<NSURL>()
+        var tags = Array<String>()
+        for tokenString in tokens {
+            if let url = NSURL.URLWithString(tokenString) {
+                urls.append(url)
+            }
+            else {
+                tags.append(tokenString)
+            }
+        }
+        return(urls, tags)
+    }
+    
     func addResource(url: NSURL) {
         var appDel = NSApplication.sharedApplication().delegate as AppDelegate
         var services = appDel.caturlogServices
